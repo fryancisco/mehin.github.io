c314b464c98407a24443c8e26fe7eb2e16861a5a
Thiago Allisson <thiago.ribeiro@ifma.edu.br>
2022-04-03 21:49:11 -0300

    Adicionando teste de tradução inicial

--- js/translate.js
+++ js/translate.js
@@ -1,13 +1,40 @@
 const CreateTranslate = () => {
-	return {
+	const canelaToPortuguese = {
+		'ah.cakà.re':	'(de: cakà.re) A.8.5. n. arbusto. bush.',
+		'ah.cakrã': '(de: cakrã) VAR: am.krãh.ti. A.4.3. vd. nublado. cloudy.',
+		'ah.cakũm':	'(de: cakũm) VAR: ah.carom [raro]. A.4.3. vd. nevoado, nevoeiro, nebuloso, neblina.',
+		'ah.cunĩ':	'(de: cunĩ) A.8.5. n. floresta virgem.',
+		'ah.kêt':	'(de: kêt) A.8.4. n. arbusto, mato, varredora ou lixo do chão.',
+		'ah.kêt':	'(de: kêt) D.5.2. vt. sujar [como um porco faz sujeira numa poça de água], confundir, misturar.',
+		'ah.kô':	'(de: ahkô) n. bicho que parece como um galho.',
+		'ah.kra':	'(de: kra) n. criança (nome geral).',
+		'ah.kra(re':	'(de: kra) D.1.7. n. criança.'
+	};
+
+	const TranslateObj = {
+		translate: (wordInCanela)	=> {
+			return canelaToPortuguese[wordInCanela] || '';
+		},
 		init: () => {
 			const form = document.getElementsByTagName('form')[0];
 			form.addEventListener('submit', (ev) => {
 				ev.preventDefault();
-				alert('Módulo de tradução em desenvolvimento...')
+				const word = document.getElementById('traduzir').value || '';
+				if(word) {
+					const translateWord = TranslateObj.translate(word) || '';
+					if(translateWord) {
+						alert(`${word} = ${translateWord}`);
+					} else {
+						alert('Palavra não encontrada no dicionário!');
+					}
+				} else {
+					alert('Digite uma palavra em Canela!');
+				}
 			});
-		}
+		}		
 	}
+
+	return TranslateObj;
 }
 
 const tl = CreateTranslate();
