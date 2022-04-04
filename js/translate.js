4e038ade6cde0bad4ff12110a4774fbde365b9aa
Francisco <51490024+fryancisco@users.noreply.github.com>
2022-04-04 13:44:41 -0300

    Update translate.js

--- js/translate.js
+++ js/translate.js
@@ -1,14 +1,14 @@
 const CreateTranslate = () => {
 	const canelaToPortuguese = {
-		'ah.cakà.re':	'(de: cakà.re) A.8.5. n. arbusto. bush.',
-		'ah.cakrã': '(de: cakrã) VAR: am.krãh.ti. A.4.3. vd. nublado. cloudy.',
+		'ah.cakà.re':	'(de: cakà.re) A.8.5. n. arbusto.',
+		'ah.cakrã': '(de: cakrã) VAR: am.krãh.ti. A.4.3. vd. nublado.',
 		'ah.cakũm':	'(de: cakũm) VAR: ah.carom [raro]. A.4.3. vd. nevoado, nevoeiro, nebuloso, neblina.',
 		'ah.cunĩ':	'(de: cunĩ) A.8.5. n. floresta virgem.',
 		'ah.kêt':	'(de: kêt) A.8.4. n. arbusto, mato, varredora ou lixo do chão.',
 		'ah.kêt':	'(de: kêt) D.5.2. vt. sujar [como um porco faz sujeira numa poça de água], confundir, misturar.',
 		'ah.kô':	'(de: ahkô) n. bicho que parece como um galho.',
 		'ah.kra':	'(de: kra) n. criança (nome geral).',
-		'ah.kra(re':	'(de: kra) D.1.7. n. criança.'
+		'ah.kra(re)':	'(de: kra) D.1.7. n. criança.'
 	};
 
 	const TranslateObj = {
